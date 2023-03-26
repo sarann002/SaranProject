27bec044c1dad5f35edcbbca0f1365b27f19f096
sarann002 <saranramesh007@gmail.com>
2023-03-26 11:23:10 +0530

    project uploaded

---
+++ src/test/java/org/test/greens/project1/SaranGreensProject.java
@@ -0,0 +1,22 @@
+package org.test.greens.project1;
+
+public class SaranGreensProject {
+
+	int a=100;
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		 
+		SaranGreensProject sr=new SaranGreensProject();
+		System.out.println("The number is===>"+"\t"+sr.a);
+		
+		
+		
+		
+		
+	}
+
+}
